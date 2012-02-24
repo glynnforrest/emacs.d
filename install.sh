b1a495fdfb33a304715c0d66be85bf0bc38ed163
glynnforrest <me@glynnforrest.com>
2012-02-24 23:22:39 +0000

    Making the install work.

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 #really crappy implementation until I learn to do it properly.
 if [ ! -h ~/.xmonad/xmonad.hs ]
 then
-	ln xmonad.hs ~/.xmonad/xmonad.hs -s
+	ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
 	echo 'Made xmonad soft link.'
 fi
