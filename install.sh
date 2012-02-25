a7619d307440c5b018203f68b9c8b044ad67791b
glynnforrest <me@glynnforrest.com>
2012-02-25 03:41:15 +0000

    Creating backups.

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ function create_link {
 	source="${PWD}/$1"
     target="${HOME}/${1/_/.}"
 
+	if [ -f ${target} ]; then
+		mv ${target} ${target}.bak
+		echo "Moving existing $target to $target.bak"
+	fi
+
 	ln -sf ${source} ${target}
 	echo "Linking $source to $target"
 
