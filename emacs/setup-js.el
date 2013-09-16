12e3f9f5966673a114a602b4448a817ca9573229
glynnforrest <me@glynnforrest.com>
2013-09-16 17:05:36 +0100

    Flycheck mode not required in js2-mode.

--- emacs/setup-js.el
+++ emacs/setup-js.el
@@ -4,7 +4,8 @@
 (setq inferior-js-program-command "env NODE_NO_READLINE=1 node")
 
 (setq js2-mode-hook (lambda ()
-                          (setq indent-tabs-mode nil)
-                          (setq js2-basic-offset 2)))
+                      (flycheck-mode -1)
+                      (setq indent-tabs-mode nil)
+                      (setq js2-basic-offset 2)))
 
 (provide 'setup-js)
