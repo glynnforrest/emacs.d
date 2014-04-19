08fa80f3f659bb0ea7388ed0aa9b2b4f5a96437a
Glynn Forrest <me@glynnforrest.com>
2014-04-19 21:44:42 +0100

    Updating php namespace snippet function.

--- emacs/setup-php.el
+++ emacs/setup-php.el
@@ -72,12 +72,14 @@ file if open."
 (defun gf/php-current-file-namespace ()
   "Get a suitable namespace for the current file."
   (interactive)
-
   (if (s-contains? "src" (buffer-file-name))
       (gf/php-namespace-from-path buffer-file-name "src")
 
     (if (s-contains? "tests" (buffer-file-name))
-        (gf/php-namespace-from-path buffer-file-name "tests"))))
+        (gf/php-namespace-from-path buffer-file-name "tests")
+
+      (if (s-contains? "app" (buffer-file-name))
+          (gf/php-namespace-from-path buffer-file-name "app")))))
 
 (defun gf/php-namespace-from-path (path substr)
   "Extract a namespace from a path name that contains `substr`."
