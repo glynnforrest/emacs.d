c098aa2da9aa4aecb251cbc93271cbdd0115157f
Glynn Forrest <me@glynnforrest.com>
2014-05-03 17:58:31 +0100

    Update web-mode bindings.

--- emacs/site-lisp/setup-web-mode.el
+++ emacs/site-lisp/setup-web-mode.el
@@ -4,8 +4,8 @@
 (define-key web-mode-map (kbd "C-c C-r") 'mc/mark-sgml-tag-pair)
 
 ;; Sort of like matchit
-(evil-declare-key 'normal web-mode-map (kbd "%") 'web-mode-tag-match)
-(evil-declare-key 'visual web-mode-map (kbd "%") 'web-mode-tag-match)
+(evil-declare-key 'normal web-mode-map (kbd "%") 'web-mode-navigate)
+(evil-declare-key 'visual web-mode-map (kbd "%") 'web-mode-navigate)
 (evil-declare-key 'normal web-mode-map (kbd ",c") 'web-mode-comment-or-uncomment)
 (evil-declare-key 'visual web-mode-map (kbd ",c") 'web-mode-comment-or-uncomment)
 
