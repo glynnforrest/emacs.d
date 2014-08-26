c05f821e3aef74b53d604a2aaa98d216ec60d38c
Glynn Forrest <me@glynnforrest.com>
2014-08-27 00:56:59 +0100

    Seriously improving project-org-file functionality.

--- emacs/site-lisp/setup-projects.el
+++ emacs/site-lisp/setup-projects.el
@@ -1,31 +1,42 @@
 (require 'projectile)
 (projectile-global-mode)
 
-(setq org-projects-dir "~/notes/projects")
+(defvar org-projects-dir (expand-file-name  "~/notes/projects"))
 
 (defun gf/create-org-path (path)
   "Create a name suitable for an org file from the last part of a file
 path."
   (let ((last (car (last (split-string (if (equal (substring path -1) "/")
-                            (substring path 0 -1) path) "/")))))
-    (downcase
-     (concat org-projects-dir "/"
+                                           (substring path 0 -1) path) "/")))))
+    (concat org-projects-dir "/"
+            (downcase
              (replace-regexp-in-string
               "\\." "-" (if (equal (substring last 0 1) ".")
-                            (substring last 1) last))
-             ".org"))))
+                            (substring last 1) last)))
+            ".org")))
+
+(defun gf/project-org-file ()
+  "Get the path of the org file for the current project."
+  (gf/create-org-path (projectile-project-root)))
 
 (defun gf/switch-to-project-org-file ()
   "Switch to the org file for the current project."
   (interactive)
-  (find-file (gf/create-org-path (projectile-project-root))))
+  (find-file (gf/project-org-file)))
+
+(defvar gf/previous-project-buffers (make-hash-table :test 'equal))
 
 (defun gf/toggle-switch-to-project-org-file ()
   "Alternate between the current buffer and the org file for the
 current project."
   (interactive)
   (if (string-equal "org-mode" (symbol-name major-mode))
-      (previous-buffer)
-    (gf/switch-to-project-org-file)))
+      (if (gethash (buffer-file-name) gf/previous-project-buffers)
+          (switch-to-buffer (gethash (buffer-file-name) gf/previous-project-buffers))
+        (error "Previous project buffer not found"))
+    (let ((file (gf/project-org-file)))
+      (puthash file (current-buffer) gf/previous-project-buffers)
+      (find-file file)
+      )))
 
 (provide 'setup-projects)
