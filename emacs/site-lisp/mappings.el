dfb947b64bb50f1c2e7e876d5e021e65e89e7039
Glynn Forrest <me@glynnforrest.com>
2014-08-27 03:56:28 +0100

    Adding flycheck next error mapping.

--- emacs/site-lisp/mappings.el
+++ emacs/site-lisp/mappings.el
@@ -39,6 +39,7 @@
 (define-key evil-normal-state-map ",w" 'save-buffer)
 (define-key evil-normal-state-map ",p" 'gf/toggle-switch-to-project-org-file)
 (define-key evil-normal-state-map ",q" 'evil-quit)
+(define-key evil-normal-state-map ",!" 'flycheck-next-error)
 (define-key evil-visual-state-map (kbd ", TAB") 'untabify)
 
 ;; Switch gj and j, gk and k
