b552126df6f43053d96ae3a38472321d249fbe5e
Glynn Forrest <me@glynnforrest.com>
2014-09-01 06:21:27 +0100

    Removing tromey package archive.

--- emacs/init.el
+++ emacs/init.el
@@ -3,7 +3,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
 (package-initialize)
 
 (when (not package-archive-contents)
