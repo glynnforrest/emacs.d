864b0494c2c1b6a12e3c2a875e530765035ba2d5
Glynn Forrest <me@glynnforrest.com>
2014-09-04 01:56:58 +0100

    Adding helm-completion to yasnippet prompt with require match.

--- emacs/site-lisp/setup-yasnippet.el
+++ emacs/site-lisp/setup-yasnippet.el
@@ -2,7 +2,21 @@
 ;; Don't use bundled snippets
 (setq yas/snippet-dirs '("~/.emacs.d/snippets"))
 (yas/global-mode 1)
-(setq yas/prompt-functions '(yas/ido-prompt yas/completing-prompt))
+
+;; cludgy hack to favor candidates over the input by setting REQUIRE-MATCH.
+(defun gf/yas-completing-prompt (prompt choices &optional display-fn completion-fn)
+  (let* ((formatted-choices
+          (if display-fn (mapcar display-fn choices) choices))
+         (chosen (funcall (or completion-fn #'completing-read)
+                          prompt formatted-choices
+                          nil t nil nil)))
+    (if (eq choices formatted-choices)
+        chosen
+      (nth (or (position chosen formatted-choices :test #'string=) 0)
+           choices))))
+
+(setq yas-prompt-functions '(gf/yas-completing-prompt))
+
 (add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
 ;; don't expand part of words
 (setq yas/key-syntaxes '("w_" "w_." "^ "))
