256bf7d247f7b295ecb881e1039911c6091bede5
Glynn Forrest <me@glynnforrest.com>
2014-11-14 12:46:53 +0000

    Disabling some flycheck features for php and js.

--- emacs/site-lisp/setup-flycheck.el
+++ emacs/site-lisp/setup-flycheck.el
@@ -5,4 +5,11 @@
 ;; checkdoc is a bit intrusive for emacs lisp configs
 (eval-after-load 'flycheck '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
 
+(eval-after-load 'flycheck '(setq flycheck-checkers (delq 'php-phpmd flycheck-checkers)))
+(eval-after-load 'flycheck '(setq flycheck-checkers (delq 'php-phpcs flycheck-checkers)))
+
+(add-hook 'js-mode-hook (lambda ()
+                         (interactive)
+                         (flycheck-mode -1)))
+
 (provide 'setup-flycheck)
