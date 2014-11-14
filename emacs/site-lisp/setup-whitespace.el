e2ed796d6d574d58441addaf8c6091ed240a4523
Glynn Forrest <me@glynnforrest.com>
2014-11-14 12:46:11 +0000

    Simplifying whitespace cleanup on save.

--- emacs/site-lisp/setup-whitespace.el
+++ emacs/site-lisp/setup-whitespace.el
@@ -25,19 +25,8 @@
 (define-key evil-normal-state-map ",=" 'gf/indent-buffer)
 (define-key evil-normal-state-map ",+" 'gf/indent-cleanup-buffer)
 
-;; Remove any trailing whitespace on buffer write
-(define-minor-mode remove-trailing-whitespace-mode
-  "Toggle remove trailing whitespace on save.
-When enabled trailing whitespace is removed before saving."
-  :init-value nil
-  :global t
-  :lighter " W"
-
-  (if remove-trailing-whitespace-mode
-      (add-hook 'before-save-hook 'delete-trailing-whitespace)
-    (remove-hook 'before-save-hook 'delete-trailing-whitespace)))
-
-(remove-trailing-whitespace-mode t)
+;; Cleanup whitespace on buffer write
+(add-hook 'before-save-hook 'whitespace-cleanup)
 
 (require 'highlight-chars)
 (add-hook 'prog-mode-hook 'hc-highlight-tabs)
