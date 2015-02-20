adfb9929ea604b8e270f11479950742634df1465
Glynn Forrest <me@glynnforrest.com>
2015-02-20 22:00:16 +0000

    Adding toggle of 2 or 4 spaces for js indent.

--- emacs/site-lisp/setup-js.el
+++ emacs/site-lisp/setup-js.el
@@ -4,9 +4,15 @@
 (setq inferior-js-program-command "env NODE_NO_READLINE=1 node")
 
 (setq js2-mode-hook (lambda ()
-                      (flycheck-mode -1)
-                      (setq indent-tabs-mode nil)
-                      (setq js2-basic-offset 2)))
+                      (flycheck-mode -1)))
+
+(defun gf/js2-mode-toggle-indent-offset ()
+  "Switch between 2 and 4 spaces for javascript indentation"
+  (interactive)
+  (if (eq js2-basic-offset 2)
+      (setq js2-basic-offset 4)
+    (setq js2-basic-offset 2))
+  (message (format "Javascript indendation is now %s spaces" js2-basic-offset)))
 
 (require 'js2-refactor)
 
