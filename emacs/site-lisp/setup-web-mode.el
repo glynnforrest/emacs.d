09c4a072c06ce4cf2fcca38ceebc5f9a105ed1b2
Glynn Forrest <me@glynnforrest.com>
2015-02-27 19:26:17 +0000

    Removing web-mode autopair for now until I have time to sort it out.

--- emacs/site-lisp/setup-web-mode.el
+++ emacs/site-lisp/setup-web-mode.el
@@ -7,6 +7,9 @@
 ;; Use multiple-cursors to rename tags
 (define-key web-mode-map (kbd "C-c C-r") 'mc/mark-sgml-tag-pair)
 
+;; Disable over zealous pairing
+(setq web-mode-enable-auto-pairing nil)
+
 ;; Sort of like matchit
 (evil-declare-key 'normal web-mode-map (kbd "%") 'web-mode-navigate)
 (evil-declare-key 'visual web-mode-map (kbd "%") 'web-mode-navigate)
