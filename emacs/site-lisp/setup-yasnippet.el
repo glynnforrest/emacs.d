4132266a859be316db1696b009a62be68e38d598
Glynn Forrest <me@glynnforrest.com>
2015-03-03 07:28:28 +0000

    Abstracting gf/in-twig-file to support any extension.

--- emacs/site-lisp/setup-yasnippet.el
+++ emacs/site-lisp/setup-yasnippet.el
@@ -31,9 +31,10 @@
   "Get the extension from a file path."
   (car (last (s-split "\\." filename))))
 
-(defun gf/in-twig-file ()
-  "Return t if the current buffer is editing a twig file."
-  (equal "twig" (gf/filename-extension (buffer-file-name))))
+(defun gf/in-file-type (extension)
+  "Return t if the extension of the current buffer file matches
+EXTENSION. Only the last extension of the file is considered."
+  (equal extension (gf/filename-extension (buffer-file-name))))
 
 (define-key yas-minor-mode-map (kbd "TAB") 'yas-expand)
 
