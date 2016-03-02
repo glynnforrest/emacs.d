4c9ab0310c3093e38cce4fdd84e7b6883ff56657
Glynn Forrest <me@glynnforrest.com>
2016-03-02 15:53:30 +0000

    Add mappings to insert php stuff in yaml files

--- emacs/site-lisp/setup-yaml.el
+++ emacs/site-lisp/setup-yaml.el
@@ -1,4 +1,8 @@
 (require 'yaml-mode)
 
 (setq yaml-indent-offset 4)
+
+(define-key yaml-mode-map (kbd "C-c I") 'gf/php-insert-class)
+(define-key yaml-mode-map (kbd "C-c s") 'gf/php-insert-service)
+
 (provide 'setup-yaml)
