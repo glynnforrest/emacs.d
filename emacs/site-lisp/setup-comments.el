cb9b2041af2dcab381d45a004c1ebd5c6570042f
Glynn Forrest <me@glynnforrest.com>
2016-11-10 15:19:27 +0000

    Load evil-nerd-commenter properly

--- emacs/site-lisp/setup-comments.el
+++ emacs/site-lisp/setup-comments.el
@@ -1,7 +1,7 @@
 (use-package evil-nerd-commenter :ensure t
   :defer t
   :commands (evilnc-comment-operator)
-  :config
+  :init
   (general-define-key
    :states '(normal visual)
    "gc" 'evilnc-comment-operator))
