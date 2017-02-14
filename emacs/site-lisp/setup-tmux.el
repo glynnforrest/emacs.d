6730fbca4db637198f5209c6cdb3af4bf3d0e475
Glynn Forrest <me@glynnforrest.com>
2017-02-14 02:54:12 +0000

    Save the buffer before executing the last tmux command

--- emacs/site-lisp/setup-tmux.el
+++ emacs/site-lisp/setup-tmux.el
@@ -1,6 +1,7 @@
 (defun gf/tmux-execute-last-command ()
   "Execute the last tmux command in the previous pane."
   (interactive)
+  (save-buffer)
   (shell-command-to-string "tmux last-pane && tmux send-keys 'up' 'Enter' && tmux last-pane"))
 
 (provide 'setup-tmux)
