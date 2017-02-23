498478ec60d1d1a495cdbefe69cddc9fba6e0afc
Glynn Forrest <me@glynnforrest.com>
2017-02-23 12:58:19 +0000

    Cleanup whitespace in markdown

--- emacs/site-lisp/setup-markdown.el
+++ emacs/site-lisp/setup-markdown.el
@@ -3,6 +3,7 @@
   (("\\.md\\'" . markdown-mode)
    ("\\.markdown\\'" . markdown-mode))
   :config
-(add-hook 'markdown-mode-hook 'flyspell-mode))
+  (add-hook 'markdown-mode-hook 'flyspell-mode)
+  (add-hook 'markdown-mode-hook 'ws-butler-mode))
 
 (provide 'setup-markdown)
