ee35f46188b58c777437f8af520efe7caf4ccea4
Glynn Forrest <me@glynnforrest.com>
2017-03-05 21:55:18 +0000

    company: fix some pain points

--- emacs/site-lisp/setup-company.el
+++ emacs/site-lisp/setup-company.el
@@ -1,11 +1,20 @@
 (use-package company :ensure t
+  :init
+  (setq company-idle-delay 0
+        company-minimum-prefix-length 2
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case nil
+        company-tooltip-limit 10)
   :config
   (global-company-mode)
+
   (general-define-key
    :keymaps '(company-active-map company-search-map company-filter-map)
    "C-j" #'company-select-next
    "C-k" #'company-select-previous
-   "C-h" #'company-show-doc-buffer)
+   "C-h" #'company-show-doc-buffer
+   "TAB" #'nil)
+
   (diminish 'company-mode))
 
 (provide 'setup-company)
