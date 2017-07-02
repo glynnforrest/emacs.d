2869caabc6118943b6a9086990f641492d3972ec
Glynn Forrest <me@glynnforrest.com>
2017-07-02 11:42:11 +0100

    js: Improve how indentation toggling works

--- emacs/site-lisp/setup-js.el
+++ emacs/site-lisp/setup-js.el
@@ -4,20 +4,25 @@
   (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
   (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
-  (setq js2-mode-hook (lambda ()
-			(flycheck-mode -1)))
+  (add-hook 'js2-mode-hook (lambda ()
+                             (flycheck-mode -1)))
+
+  (setq-default js2-basic-offset 2)
 
   (defun gf/js2-mode-toggle-indent-offset ()
     "Switch between 2 and 4 spaces for javascript indentation"
     (interactive)
-    (if (eq js2-basic-offset 2)
-	(setq js2-basic-offset 4)
-      (setq js2-basic-offset 2))
-    (message (format "Javascript indentation is now %s spaces" js2-basic-offset)))
+    (if (eq (default-value 'js2-basic-offset) 4)
+        (setq-default js2-basic-offset 2)
+      (setq-default js2-basic-offset 4))
+    (message (format "Default javascript indentation is now %s spaces" (default-value 'js2-basic-offset))))
 
-  (setq js2-basic-offset 2)
-
-  )
+  (general-define-key
+   :states '(normal visual insert emacs)
+   :prefix "SPC"
+   :non-normal-prefix "M-SPC"
+   :keymaps 'js2-mode-map
+   "ti" '(gf/js2-mode-toggle-indent-offset :which-key "javascript indentation")))
 
 (use-package js2-refactor :ensure t
   :after js2-mode)
