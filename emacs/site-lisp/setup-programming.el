124c23134169bdee90d3d493b3d6694710c47257
Glynn Forrest <me@glynnforrest.com>
2017-07-02 15:30:47 +0100

    Cleanup dired and some old code

--- emacs/site-lisp/setup-programming.el
+++
@@ -1,9 +0,0 @@
-(require 'emamux)
-(setq emamux:use-nearest-pane t)
-
-;; Overriding this function so Emacs.app on OSX works.
-(defun emamux:in-tmux-p () t)
-
-(require 'setup-eshell)
-
-(provide 'setup-programming)
