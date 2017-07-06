b0a2109b609794ab8cf069070c73466277533341
Glynn Forrest <me@glynnforrest.com>
2017-07-06 11:34:47 +0100

    tmux: Improve how the target pane is selected

--- emacs/site-lisp/setup-tmux.el
+++ emacs/site-lisp/setup-tmux.el
@@ -1,22 +1,52 @@
-(defvar gf/tmux-previous-cmd nil)
+(defvar gf/tmux--target-pane nil)
+(defvar gf/tmux--previous-cmd nil)
 
-(defun gf/tmux-do-run (cmd)
-  "Run a command in the previous tmux pane."
+(defun gf/tmux--list-panes ()
+  (split-string (shell-command-to-string
+                 "tmux list-panes -a -F \"#{session_name}:#{window_index}.#{pane_index} #{pane_current_command} in #{pane_current_path}\" | column -t") "\n"))
+
+(defun gf/tmux--set-target-pane ()
+  "Set the target pane to run tmux commands in."
+  (setq gf/tmux--target-pane
+        (car (split-string
+              (completing-read "Select target tmux pane: " (gf/tmux--list-panes) nil t)
+              " "))))
+
+(defun gf/tmux--get-target-pane ()
+  "Get the target pane to run tmux commands in."
+  (when (or (not gf/tmux--target-pane) current-prefix-arg)
+    (gf/tmux--set-target-pane))
+  gf/tmux--target-pane)
+
+(defun gf/tmux--create-command (cmd)
+  (let ((target (gf/tmux--get-target-pane)))
+    (format "tmux send-keys -t %s -l '%s' && tmux send-keys -t %s Enter" target cmd target)))
+
+(defun gf/tmux--do-run (cmd)
   (save-buffer)
-  (setq gf/tmux-previous-cmd cmd)
-  (shell-command-to-string (concat "tmux last-pane && tmux send-keys -l '" cmd "' && tmux send-keys 'Enter' && tmux last-pane")))
+  (setq gf/tmux--previous-cmd cmd)
+  (shell-command-to-string
+   (gf/tmux--create-command cmd)))
+
 
 (defun gf/tmux-run (cmd)
-  "Run a tmux command in the previous tmux pane."
+  "Run a command in the target tmux pane."
   (interactive "MCommand: ")
-  (gf/tmux-do-run cmd))
+  (gf/tmux--do-run cmd))
 
 (defun gf/tmux-run-last ()
   "Run the last command in the previous tmux pane."
   (interactive)
-  (if gf/tmux-previous-cmd
-      (gf/tmux-do-run gf/tmux-previous-cmd)
-    (call-interactively 'gf/tmux-execute-command)))
+  (if gf/tmux--previous-cmd
+      (gf/tmux--do-run gf/tmux--previous-cmd)
+    (call-interactively 'gf/tmux-run)))
+
+(defun gf/tmux-reset ()
+  "Reset the target pane and last command."
+  (interactive)
+  (setq gf/tmux--target-pane nil
+        gf/tmux--previous-cmd nil))
+
 
 (defmacro gf/tmux-def-command (name command key)
   "Create a new function gf/tmux-command-NAME and bind it to SPC c `KEY'."
