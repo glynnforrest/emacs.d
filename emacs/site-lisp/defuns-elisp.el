64dc286607d857cd3f8531a34026a30b768269ff
Glynn Forrest <me@glynnforrest.com>
2017-12-05 03:17:00 +0000

    Move more code into defuns-elisp

--- emacs/site-lisp/defuns-elisp.el
+++ emacs/site-lisp/defuns-elisp.el
@@ -1,3 +1,4 @@
+;; pp-macroexpand-last-sexp does the same, but isn't read-only and writes a noisy message
 (defun gf/show-previous-sexp-macro-expand ()
   "Show the result of macro expanding the previous sexp."
   (interactive)
@@ -7,5 +8,27 @@
    (pp lisp)
    (emacs-lisp-mode)
    (read-only-mode))))
+
+(defun lisp-describe-thing-at-point ()
+  "Show the documentation of the Elisp function and variable near point.
+   This checks in turn:
+     -- for a function name where point is
+     -- for a variable name where point is
+     -- for a surrounding function call"
+  (interactive)
+  (let (sym)
+    ;; sigh, function-at-point is too clever.  we want only the first half.
+    (cond ((setq sym (ignore-errors
+                       (with-syntax-table emacs-lisp-mode-syntax-table
+                         (save-excursion
+                           (or (not (zerop (skip-syntax-backward "_w")))
+                               (eq (char-syntax (char-after (point))) ?w)
+                               (eq (char-syntax (char-after (point))) ?_)
+                               (forward-sexp -1))
+                           (skip-chars-forward "`'")
+                           (let ((obj (read (current-buffer))))
+                             (and (symbolp obj) (fboundp obj) obj))))))
+           (describe-function sym))
+          ((setq sym (variable-at-point)) (describe-variable sym)))))
 
 (provide 'defuns-elisp)
--- emacs/site-lisp/setup-elisp.el
+++ emacs/site-lisp/setup-elisp.el
@@ -36,36 +36,4 @@
 
 (require 'defuns-elisp)
 
-(defun ielm-auto-complete ()
-  "Enables `auto-complete' support in \\[ielm]."
-  (setq ac-sources '(ac-source-functions
-                     ac-source-variables
-                     ac-source-features
-                     ac-source-symbols
-                     ac-source-words-in-same-mode-buffers))
-  (add-to-list 'ac-modes 'inferior-emacs-lisp-mode)
-  (auto-complete-mode 1))
-
-(defun lisp-describe-thing-at-point ()
-  "Show the documentation of the Elisp function and variable near point.
-   This checks in turn:
-     -- for a function name where point is
-     -- for a variable name where point is
-     -- for a surrounding function call"
-  (interactive)
-  (let (sym)
-    ;; sigh, function-at-point is too clever.  we want only the first half.
-    (cond ((setq sym (ignore-errors
-                       (with-syntax-table emacs-lisp-mode-syntax-table
-                         (save-excursion
-                           (or (not (zerop (skip-syntax-backward "_w")))
-                               (eq (char-syntax (char-after (point))) ?w)
-                               (eq (char-syntax (char-after (point))) ?_)
-                               (forward-sexp -1))
-                           (skip-chars-forward "`'")
-                           (let ((obj (read (current-buffer))))
-                             (and (symbolp obj) (fboundp obj) obj))))))
-           (describe-function sym))
-          ((setq sym (variable-at-point)) (describe-variable sym)))))
-
 (provide 'setup-elisp)
