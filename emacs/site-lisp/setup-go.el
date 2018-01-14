b0b061deb843e1bb52dc9a90d95e0543d10353f1
Glynn Forrest <me@glynnforrest.com>
2018-01-14 01:03:33 +0000

    Revert "Only run gofmt hook on go buffers"
    This reverts commit bb746415d32f6fff640f87a7d050f43d4c0aff58.

--- emacs/site-lisp/setup-go.el
+++ emacs/site-lisp/setup-go.el
@@ -1,6 +1,6 @@
 (use-package go-mode :ensure t
   :config
   (progn
-      (add-hook 'before-save-hook 'gofmt-before-save nil t)))
+      (add-hook 'before-save-hook 'gofmt-before-save)))
 
 (provide 'setup-go)
