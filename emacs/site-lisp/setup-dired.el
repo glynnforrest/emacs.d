97184d5133a6fb7abfba7fb2c272411a35c4e1ba
Glynn Forrest <me@glynnforrest.com>
2018-04-22 11:44:55 +0000

    Don't lose spacebar mappings in dired mode

--- emacs/site-lisp/setup-dired.el
+++ emacs/site-lisp/setup-dired.el
@@ -2,6 +2,9 @@
   :defer t
   :config
   (setq dired-recursive-copies 'always
-        dired-recursive-deletes 'top))
+        dired-recursive-deletes 'top)
+  (general-define-key
+   :keymaps 'dired-mode-map
+   "SPC" nil))
 
 (provide 'setup-dired)
