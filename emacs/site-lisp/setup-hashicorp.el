95ba4ee7fa96ac3b1f1e0632dcfa5a44fad7531a
Glynn Forrest <me@glynnforrest.com>
2020-02-09 12:02:14 +0100

    Format terraform buffers with SPC +

--- emacs/site-lisp/setup-hashicorp.el
+++ emacs/site-lisp/setup-hashicorp.el
@@ -1,5 +1,15 @@
 (use-package hcl-mode :ensure t)
 
-(use-package terraform-mode :ensure t)
+(use-package terraform-mode
+  :ensure t
+  :config
+
+  (general-define-key
+   :states '(normal visual insert emacs)
+   :prefix "SPC"
+   :non-normal-prefix "M-SPC"
+   :keymaps 'terraform-mode-map
+   "+" 'terraform-format-buffer)
+  )
 
 (provide 'setup-hashicorp)
