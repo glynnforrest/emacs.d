fe5d7b13629e1a69963c687554c61065db94f507
Glynn Forrest <me@glynnforrest.com>
2023-04-27 10:05:00 -0500

    Make markdown more like org-mode

--- emacs/site-lisp/setup-markdown.el
+++ emacs/site-lisp/setup-markdown.el
@@ -1,9 +1,27 @@
+(eval-when-compile (require 'use-package))
+
 (use-package markdown-mode
   :mode
   (("\\.md\\'" . markdown-mode)
    ("\\.markdown\\'" . markdown-mode))
   :config
+  (setq markdown-asymmetric-header t)
+
+  (general-define-key
+   :keymaps 'markdown-mode-map
+   "M-h" 'markdown-promote-subtree
+   "M-l" 'markdown-demote-subtree
+   "M-H" 'markdown-promote
+   "M-L" 'markdown-demote
+   "M-RET" 'markdown-insert-header-dwim)
+
+  (general-define-key
+   :keymaps 'markdown-mode-map
+   :states 'normal
+   "TAB" 'outline-cycle)
+
   (add-hook 'markdown-mode-hook 'flyspell-mode)
-  (add-hook 'markdown-mode-hook 'ws-butler-mode))
+  (add-hook 'markdown-mode-hook 'ws-butler-mode)
+  (add-hook 'markdown-mode-hook 'outline-minor-mode))
 
 (provide 'setup-markdown)
