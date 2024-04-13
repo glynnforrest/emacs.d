3021ac1a9d876b8daa53ea654a16fa8817f5f871
Glynn Forrest <me@glynnforrest.com>
2024-04-12 21:23:36 -0700

    Use treesitter for python

--- site-lisp/setup-python.el
+++ site-lisp/setup-python.el
@@ -1,9 +1,11 @@
 (eval-when-compile (require 'use-package))
 
+(add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
+
 (use-package lsp-python-ms
   :ensure t
   :init (setq lsp-python-ms-auto-install-server t)
-  :hook (python-mode . (lambda ()
+  :hook (python-ts-mode . (lambda ()
                           (require 'lsp-python-ms)
                           (lsp-deferred))))
 
